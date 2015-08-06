dfdb5d853addfbd044a625d89ee2fd341114599c
Benjamin M. Brown <benjaminbb@gmail.com>
2015-08-06 11:24:12 -0500

    Set up node packages, initial restify server setup

---
+++ dev/server.js
@@ -0,0 +1,23 @@
+var restify = require('restify');
+var mongojs = require('mongojs');
+var morgan = require('morgan');
+var db = mongojs('bucketlist', ['appUsers', 'bucketLists']);
+var server = restify.createServer();
+
+//restify middleware
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser);
+server.use(restify.bodyParser);
+server.use(morgan('dev'));//LOGGING
+
+//CORS
+server.use(function(req,res,next){
+res.header('Access-Control-Allow-Origin', "*");
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'Content-Type');
+next();
+});
+
+server.listen(process.env.PORT || 9804, function(){
+	console.log("Server started on port:", process.env.PORT || 9804);s
+});
