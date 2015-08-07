b09dc843a41c3e1108c38e1c0cdc151cbd438e09
Benjamin M. Brown <benjaminbb@gmail.com>
2015-08-07 14:27:37 -0500

    Add email validations

--- dev/server.js
+++ dev/server.js
@@ -2,7 +2,10 @@ var restify = require('restify');
 var mongojs = require('mongojs');
 var morgan = require('morgan');
 var db = mongojs('bucketlist', ['appUsers', 'bucketLists']);
+
 var server = restify.createServer();
+
+var manageUsers = require('../auth/manageUser')(server,db);
 
 //restify middleware
 server.use(restify.acceptParser(server.acceptable));
@@ -19,5 +22,5 @@ next();
 });
 
 server.listen(process.env.PORT || 9804, function(){
-	console.log("Server started on port:", process.env.PORT || 9804);s
+	console.log("Server started on port:", process.env.PORT || 9804);
 });
